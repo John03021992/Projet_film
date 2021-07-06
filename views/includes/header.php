94cd554df0951959d3ff2733555e2f048cb613e8
Jonathan Juillerat <john432@hotmail.fr>
2021-07-06 13:49:15 +0200

    Ajout de l'organisation de base des dossiers

---
+++ views/includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home - Simple rooting with PHP</title>
+</head>
+<body>
+
+<header>
+    <!-- <ul>
+      <li><a href="index.php?page=home">Accueil</a></li>
+      <li><a href="index.php?page=contact">Contact</a></li>
+    </ul> -->
+</header>
