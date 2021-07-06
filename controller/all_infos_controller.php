66fd19128187293228450855e3f063ed6f95c5b8
“laury <lquarto.lq@gmail.com>
2021-07-06 14:00:40 +0200

    supp ancienne version

--- controller/all_infos_controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require('../model/all_infos_model.php');
-
-
-require_once('../vue/all_infos_vue.php');
-
-?>
--- vue/categories_vue.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    require ('../controller/categories_controller.php');
-?>
-    
-<p>
-    <?php foreach($catFilms as $catFilm): ?>
-    <h1><?php echo $catFilm["titre"]?></h1><?php endforeach; ?>
-</p>
